bf0e4b971b4f602e1889cb9dda84ec98a835e636
Rus1130 <arirogozi@gmail.com>
2023-08-28 17:50:42 -0400

    subunit 3

---
+++ Unit 1/Subunit 3/1.3.4 Naming Variables.java
@@ -0,0 +1,16 @@
+// 1.3.4.1
+/*public*/ class CaseSensitiveClass
+{
+    public static void main(String[] args)
+    {
+        int gameScore = 0; // variable name using camel case
+        // this is using a different variable without camel case!
+        System.out.println("gameScore is " + gameScore);
+    }
+}
+
+// 1.3.4.2
+// shoeSize
+
+// 1.3.4.3
+// topScore
---
+++ Unit 1/Subunit 3/1.3.5 Debugging Challenge.java
@@ -0,0 +1,17 @@
+// 1.3.5.1
+/*public*/ class Challenge1_3
+{
+   public static void main(String[] args)
+   {
+       double temperature = 70.5;
+       int tvChannel = 101;
+       boolean sunny = true;
+
+       System.out.print("Welcome to the weather report on Channel ");
+       System.out.println(tvChannel);
+       System.out.print("The temperature today is ");
+       System.out.println(temperature);
+       System.out.print("Is it sunny today? ");
+       System.out.println(sunny);
+   }
+}
