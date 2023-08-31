bf439f48fc27542a4a749ac301638814ef83d249
Rus1130 <arirogozi@gmail.com>
2023-08-30 22:58:58 -0400

    completed unit 1

---
+++ Unit 1/Subunit 5/1.5 Compound Assignment Operators.java
@@ -0,0 +1,22 @@
+// 1.5.1
+/*public*/ class Test2
+{
+    public static void main(String[] args)
+    {
+        int score = 0;
+        System.out.println(score);
+        score++;
+        System.out.println(score);
+        score *= 2;
+        System.out.println(score);
+        int penalty = 5;
+        score -= penalty / 2;
+        System.out.println(score);
+    }
+}
+
+// 1.5.2
+// E. x = -1, y = 2, z = 4
+
+// 1.5.3
+// E. x = 4, y = 2, z = 3
---
+++ Unit 1/Subunit 6/1.6.1 Programming Challenge.java
@@ -0,0 +1,29 @@
+// 1.6.1.1
+/*public*/ class Challenge1_6
+{
+    public static void main(String[] args)
+    {
+        // 1. Declare 3 int variables called grade1, grade2, grade3
+        // and initialize them to 3 values
+        double grade1 = 90.0;
+        double grade2 = 100.0;
+        double grade3 = 94.0;
+
+        // 2. Declare an int variable called sum for the sum of the grades
+        double sum = 0.0;
+
+        // 3. Declare a variable called average for the average of the grades
+        double average = 0.0;
+
+        // 4. Write a formula to calculate the sum of the 3 grades (add them
+        // up).
+        sum = (double) grade1 + grade2 + grade3;
+
+        // 5. Write a formula to calculate the average of the 3 grades from
+        // the sum using division and type casting.
+        average = sum / 3;
+
+        // 6. Print out the average
+        System.out.println(average);
+    }
+}
